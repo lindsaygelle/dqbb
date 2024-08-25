e4ce5f8e8edf8b5dc9d18b63ba3682991f885414
lindsaygelle <lindsay.gelle@gmail.com>
2024-08-26 09:49:39 +1000

    Dev

---
+++ src/test/kotlin/TestHealApplier.kt
@@ -0,0 +1,55 @@
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+internal class TestHealApplier {
+    private class HealApplier : dqbb.HealApplier {
+        override var healRangeMaximum: Int = 0
+        override var healRangeMinimum: Int = 0
+        override var healScale: Int = 0
+        override var healShift: Int = 0
+    }
+
+    private lateinit var healApplier: dqbb.HealApplier
+
+    @BeforeTest
+    fun before() {
+        healApplier = HealApplier()
+    }
+
+    @Test
+    fun testHealScale() {
+        val healScale = 0x0A // 10
+        healApplier.healScale = healScale
+        assertEquals(healScale, healApplier.healScale)
+    }
+
+    @Test
+    fun testHealShift() {
+        val healShift = 0x07 // 7
+        healApplier.healShift = healShift
+        assertEquals(healShift, healApplier.healShift)
+    }
+
+    @Test
+    fun testHeal() {
+        // Enemy Heal
+        healApplier.healScale = 0x0A // 10
+        healApplier.healShift = 0x07 // 7
+        for (i in (0..255)) {
+            val heal = healApplier.heal
+            healApplier.healRangeMaximum = i
+            assertContains((10..17), heal)
+        }
+        // Hero Heal
+        healApplier.healScale = 0x14
+        healApplier.healShift = 0x07
+        for (i in (0..255)) {
+            val heal = healApplier.heal
+            healApplier.healRangeMaximum = i
+            assertContains((20..27), heal)
+        }
+
+    }
+}
